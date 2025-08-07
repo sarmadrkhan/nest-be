ce497cf42fd160d4319edb4210f778643cfcc4f1
sarmadrkhan <raeessarmad200@gmail.com>
2025-08-07 22:18:25 +0200

    configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import open from 'open';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('API documentation for the application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   const port = configService.get('PORT') || 2026;
 
   await app.listen(port);
+  if (process.env.NODE_ENV === 'development') {
+    await open(`http://localhost:${port}/api/docs`);
+  }
 }
 bootstrap();
