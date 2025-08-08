507c58f43a1a633db99ce3b284db91e6a0ad488f
sarmadrkhan <raeessarmad200@gmail.com>
2025-08-08 16:14:51 +0200

    use auth dto in controller with validation and swagger config

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  @ApiBody({ type: AuthDto })
+  signup(@Body() body: AuthDto) {
+    console.log({body});
     return this.authService.signup();
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   signup() {
-    return 'Signup';
+    return { message: 'Signup successful' };
   }
 
   signin() {
-    return 'Signin';
+    return { message: 'Signin' };
   }
 }
