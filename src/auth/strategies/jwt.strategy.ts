2bb559cb7175d499235bbb6b71b46315506db279
sarmadrkhan <raeessarmad200@gmail.com>
2025-08-17 01:20:31 +0200

    define jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    const jwtSecret = config.get("JWT_SECRET");
+    
+    if (!jwtSecret) {
+      throw new Error("JWT_SECRET is not defined in the configuration");
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+    });    
+  }
+
+  async validate(payload: { sub: string; email: string }) {
+    return { userId: payload.sub, email: payload.email };
+  }
+}
