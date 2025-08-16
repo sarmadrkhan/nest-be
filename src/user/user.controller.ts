5db763af7b7f0dd125aa37fe2ff8a312030a78e7
sarmadrkhan <raeessarmad200@gmail.com>
2025-08-17 01:37:30 +0200

    define a protected route and configure swagger for auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { ApiBearerAuth, ApiOperation } from "@nestjs/swagger";
 
 @Controller('users')
 export class UserController {
+
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get current user profile' })
+  @Get('me')
+  getMe() {
+    return { message: "User profile data" };
+  }
 }
