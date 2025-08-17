b4d9432e0d5ca6e4c4be941062159dc71d876d76
sarmadrkhan <raeessarmad200@gmail.com>
2025-08-17 02:21:30 +0200

    get userinfo from db on validaion and remove the password hash before returning

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,24 +1,33 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
-    const jwtSecret = config.get("JWT_SECRET");
-    
+  constructor(
+    config: ConfigService,
+    private readonly prisma: PrismaService,
+  ) {
+    const jwtSecret = config.get('JWT_SECRET');
+
     if (!jwtSecret) {
-      throw new Error("JWT_SECRET is not defined in the configuration");
+      throw new Error('JWT_SECRET is not defined in the configuration');
     }
 
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: jwtSecret,
-    });    
+    });
   }
 
   async validate(payload: { sub: string; email: string }) {
-    return { userId: payload.sub, email: payload.email };
+    const user = await this.prisma.user.findUniqueOrThrow({
+      where: { id: payload.sub },
+    });
+
+    const { hash, ...safeUser } = user;
+    return safeUser;
   }
 }
