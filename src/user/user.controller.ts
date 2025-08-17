abf63156af9c37c521db1c956ea2db68a54114b0
sarmadrkhan <raeessarmad200@gmail.com>
2025-08-17 02:43:58 +0200

    move guard to controller level to use with all routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
+@ApiBearerAuth()
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
-  @ApiBearerAuth()
-  @ApiOperation({ summary: 'Get current user profile' })
+  
   @Get('me')
+  @ApiOperation({ summary: 'Get current user profile' })
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch('update')
+  @ApiOperation({ summary: 'Update current user profile' })
+  updateProfile() {
+    return { message: 'Profile updated successfully' };
   }
 }
