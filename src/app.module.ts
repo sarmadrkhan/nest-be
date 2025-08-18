cff995b96a839fa2e992f03bd6e9e5f36b0c2e18
sarmadrkhan <raeessarmad200@gmail.com>
2025-08-18 14:52:43 +0200

    configure schedule module and cleanup service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CleanupService } from './common/jobs';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
     AuthModule,
     UserModule,
     PrismaModule,
   ],
+  providers: [CleanupService],
 })
 export class AppModule { }
